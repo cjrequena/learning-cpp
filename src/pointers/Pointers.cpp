b77c57ef2c16abaa07edcaa18ccf162d17e42ec7
Carlos Requena <requenacj@gmail.com>
2019-08-14 15:25:48 +0200

    pointers

---
+++ src/pointers/Pointers.cpp
@@ -0,0 +1,5 @@
+//
+// Created by carlosjose.requena on 14/08/2019.
+//
+
+#include "Pointers.hpp"
---
+++ src/pointers/Pointers.hpp
@@ -0,0 +1,12 @@
+//
+// Created by carlosjose.requena on 14/08/2019.
+//
+
+#ifndef CPP_EXERCISES_SRC_POINTERS_POINTERS_HPP_
+#define CPP_EXERCISES_SRC_POINTERS_POINTERS_HPP_
+
+class Pointers {
+
+};
+
+#endif //CPP_EXERCISES_SRC_POINTERS_POINTERS_HPP_
