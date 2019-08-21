651f223227506a7dc7381d5922b9d1f75bc4a7fd
Carlos Requena <requenacj@gmail.com>
2019-08-21 15:07:04 +0200

    storage classes

---
+++ src/storageclasses/StorageClasses.hpp
@@ -0,0 +1,14 @@
+//
+// Created by carlosjose.requena on 21/08/2019.
+//
+
+#ifndef CPP_EXERCISES_SRC_STORAGECLASSES_STORAGECLASSES_HPP_
+#define CPP_EXERCISES_SRC_STORAGECLASSES_STORAGECLASSES_HPP_
+
+class StorageClasses {
+ public:
+  StorageClasses();
+  ~StorageClasses();
+};
+
+#endif //CPP_EXERCISES_SRC_STORAGECLASSES_STORAGECLASSES_HPP_
